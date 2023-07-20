2a3bd62acfb7309e6f4d03e4fe4e7c9df03608c6
chirag900155 <139253586+chirag900155@users.noreply.github.com>
2023-07-20 22:27:57 +0530

    Update connect.js

--- Server/connect.js
+++ Server/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function connectMongodb(){
-    const URI = `mongodb+srv://ChiragGwalani:Chirag123@cluster0.6smf1n8.mongodb.net/SocialMediaApis?retryWrites=true&w=majority`
+    const URI = `mongodb+srv://<UserName>:<Your Pssword>@cluster0.6smf1n8.mongodb.net/SocialMediaApis?retryWrites=true&w=majority`
 
     try{
         await mongoose.connect(URI, {
